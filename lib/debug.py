7a39800411ab8c13121473860a1c254e73ae2ba6
Marvin Kadoyo <kadoyomarvin@gmail.com>
2025-05-27 19:49:51 +0300

    Add debug script for interactive testing of models and relationships

--- lib/debug.py
+++ lib/debug.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
 from sqlalchemy import create_engine
-
-from models import Company, Dev
+from models import Base, Company, Dev
 
 if __name__ == '__main__':
     engine = create_engine('sqlite:///freebies.db')
-    import ipdb; ipdb.set_trace()
+    Base.metadata.create_all(engine)  # This creates the tables based on your models
+    print("Tables created.")
